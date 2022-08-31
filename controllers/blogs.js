1b97386a59afc177151c141342c76330f0a1fc0f
pukinuki <fluqueo@gmail.com>
2022-08-31 17:50:49 +0200

    Exercises 4.16-4.18

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
 
@@ -17,8 +18,18 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/',async (request, response) => {
 
-  const blog = new Blog(request.body)
+  const body = { ...request.body }
+
+  const user = await User.findOne({})
+
+  body['user'] = user._id
+
+  const blog = new Blog(body)
+
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
@@ -6,25 +7,27 @@ const User = require('../models/user')
 
 const api = supertest(app)
 
-describe('Api users with initial database tests that', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
+beforeEach(async () => {
+  await User.deleteMany({})
 
-    let userObjects = [...helper.users]
+  const passwordHashList = []
 
-    const hashPassword = async (password) => await bcrypt.hash(password, 10)
-    userObjects = userObjects.map(user => {
-      console.log(Promise. hashPassword(user.password))
-      user['passwordHash'] = 'secret'
-      delete user['password']
-      return new User(user)
-    })
+  for (let i = 0; i < helper.users.length; i++) {
+    passwordHashList[i] = await bcrypt.hash(helper.users[i].password, 10)
+  }
 
-    console.log(userObjects)
-
-    const promiseArray = userObjects.map(user => user.save())
-    await Promise.all(promiseArray)
+  const userArrayCopy = helper.users.map(user => ({ ...user }))
+  const userObjects = userArrayCopy.map((user, index) => {
+    user['passwordHash'] = passwordHashList[index]
+    delete user['password']
+    return new User(user)
   })
+
+  const promiseArray = userObjects.map(user => user.save())
+  await Promise.all(promiseArray)
+})
+
+describe('Api users with initial database tests that', () => {
 
   test('creation succeeds with a fresh username', async () => {
     const usersAtStart = await helper.usersInDb()
@@ -47,4 +50,72 @@ describe('Api users with initial database tests that', () => {
     const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain(newUser.username)
   })
+
+  test('users with "username" already in database are not created and return message', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newInvalidUser = {
+      username: 'test1',
+      name: 'Test 1 already in database',
+      password: 'test1',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newInvalidUser)
+      .expect(400)
+
+    expect(response.body.error).toContain('username must be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
+
+  test('users with invalid username are not created and return message', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newInvalidUser = {
+      username: 'ff',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newInvalidUser)
+      .expect(400)
+
+    expect(response.body.error).toContain('User validation failed: username')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
+
+  test('users with invalid password are not created and return message', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newInvalidUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'sa',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newInvalidUser)
+      .expect(400)
+
+    expect(response.body.error).toContain('password must be at least 3 characters long')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
+
+})
+
+afterAll(() => {
+  mongoose.connection.close()
 })
